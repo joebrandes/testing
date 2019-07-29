bad22f770c3e81f93e2b109fd2302fac6b50b5f0
Joe Brandes <joe.brandes@gmail.com>
2019-07-29 16:44:31 +0200

    here second dev made some changes in B (Beatrice)

--- output.c
+++ output.c
@@ -1,6 +1,9 @@
 int i;
 
-for(i = 0; i < nr_of_lines(); i++)
+for(i = 0; i < nr_of_lines(); i++) {
+    if(i > config_get("max_output_lines"))
+        break;
     output_line(i);
+}
 
 print_stats();
